f279256660ed5120b4595af09aab18d507aac298
ArmanJscript <armandaneshdoost@gmail.com>
2023-05-18 20:42:56 +0330

    Add files via upload

--- Chrome-Extensions/YoutubeDlExtension/server/index.js
+++ Chrome-Extensions/YoutubeDlExtension/server/index.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const { validate, Joi } = require("express-validation");
+const ytdl = require('ytdl-core');
+const cors = require('cors');
 
+
+app = express();
+app.use(cors());
+
+
+app.get('/download', (req, res) => {
+
+    const url = req.query.url;
+    const quality = req.query.quality;
+    const format = req.query.format;
+
+    video = ytdl(url, {
+        format: format,
+        quality: quality
+    }).pipe(res);
+});
+
+
+const port = 4000;
+
+const start = () => {
+    app.listen(port, () => {
+        console.log(`Server Started on port ${port}`);
+    });
+};
+
+start();
