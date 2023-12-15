49c4380dfe3c4801d2f86429104e278e7a97061f
armanjscript <armandaneshdoost@gmail.com>
2023-12-16 00:08:47 +0330

    LinkedLists finally approved

--- DataStructures/Section3/LinkedLists.py
+++ DataStructures/Section3/LinkedLists.py
@@ -71,6 +71,54 @@ class SinglyLinkedList:
                 val = current.data
                 current = current.next
                 yield val
+    
+    def search(self, data):
+        for node in self.iter():
+            if node == data:
+                return True
+        return False
+    def length(self):
+        return self.size
+    
+    def delete_first(self):
+        current = self.head
+        if self.head is None:
+            print("No items here")
+        else:
+            self.head = current.next
+            self.size -=1
+    
+    def delete_last(self):
+        current = self.head
+        prev = self.head
+
+        while current:
+            if current.next is None:
+                prev.next = current.next
+                self.size -=1
+            prev = current
+            current = current.next
+    
+    def delete(self, data):
+
+        current = self.head
+        prev = self.head
+
+        while current:
+            if current.data == data:
+                if current == self.head:
+                    self.head = current.next
+                else:
+                    prev.next = current.next
+                self.size -=1
+                return
+            prev = current
+            current = current.next
+    
+    def clear(self):
+        self.tail = None
+        self.head = None
+        self.size = 0
 
 print("++++++++++++++++++")
 words = SinglyLinkedList()
@@ -78,7 +126,19 @@ words.append('Python')
 words.append('Java')
 words.append('C++')
 words.append('C#')
-words.append_loc('Javascript', 3)
+words.append_loc('Javascript', 3) #append item to specific location in the linkedlist
+words.delete_first()
+words.delete_last()
+words.delete('Java')
+
+for word in words.iter():
+    print(word)
+
+print(words.search('Python'))
+print(words.length())
+
+
+words.clear()
 
 for word in words.iter():
     print(word)
