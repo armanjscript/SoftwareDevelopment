7e0e1e164a49812b0560238677e208b51b8566ba
armanjscript <armandaneshdoost@gmail.com>
2024-01-06 07:51:03 +0330

    Minheap.py finally approved

--- DataStructures/Section6/Minheap.py
+++ DataStructures/Section6/Minheap.py
@@ -15,7 +15,7 @@ class MinHeap:
         self.arrange(self.size)
     
     def minchild(self, k):
-        if k*2 > self.size:
+        if k*2+1> self.size:
             return k*2
         elif self.heap[k*2] < self.heap[k*2+1]:
             return k*2
@@ -37,6 +37,21 @@ class MinHeap:
         self.sink(1)
         return item
 
+    def delete_at_location(self, location):
+        item = self.heap[location]
+        self.heap[location] = self.heap[self.size]
+        self.size-=1
+        self.heap.pop()
+        self.sink(location)
+        return item
+    
+    def heap_sort(self): 
+        sorted_list = [] 
+        for node in range(self.size): 
+            n = self.delete_at_root() 
+            sorted_list.append(n) 
+        return sorted_list
+
     
 
 
@@ -56,3 +71,25 @@ print(h.heap)
 n = h.delete_at_root()
 print(n)
 print(h.heap)
+
+print("++++++++++++++++++++++++\n")
+
+h = MinHeap()  
+for i in (4, 8, 7, 2, 9, 10, 5, 1, 3, 6):  
+    h.insert(i)    
+print(h.heap)
+ 
+n = h.delete_at_location(2)
+print(n)
+print(h.heap)
+
+print("Now HeapSort:=============\n")
+h = MinHeap() 
+unsorted_list = [4, 8, 7, 2, 9, 10, 5, 1, 3, 6] 
+for i in unsorted_list: 
+    h.insert(i) 
+print("Unsorted list: {}".format(unsorted_list))
+
+
+print("Unsorted list: {}".format(unsorted_list))
+print("Sorted list: {}".format(h.heap_sort()))
