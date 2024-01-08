097fdac0fc08fb69606b85a20263a4c9c418383f
armanjscript <armandaneshdoost@gmail.com>
2024-01-08 08:12:57 +0330

    HashTables.py changed

--- DataStructures/Section7/HashTables.py
+++ DataStructures/Section7/HashTables.py
@@ -70,6 +70,47 @@ class HashTable:
         self.size = new_hash_table.size
         self.slots = new_hash_table.slots
     
+    def get(self, key):
+        h = self._hash(key)
+        while self.slots[h]!=None:
+            if self.slots[h].key == key:
+                return self.slots[h].value
+            h = (h+1)%self.size
+        return None
+
+    #implement hashtable as dictionary
+    def __setitem__(self, key, value):
+        self.put(key, value)
+    
+    def __getitem__(self, key):
+        return self.get(key)
+
+    #quadratic probing vs linear probing
+    def get_quadratic(self, key):
+        h = self._hash(key)
+        j = 1
+        while self.slots[h] != None:
+            if self.slots[h].key == key:
+                return self.slots[h].value
+            h = (h+ j**2) % self.size
+            j = j + 1
+        return None
+    
+    def put_quadratic(self, key, value):
+        item = HashItem(key, value)
+        h = self._hash(key)
+        j = 1
+        while self.slots[h] != None:
+            if self.slots[h].key == key:
+                break
+            h = (h + j*j) % self.size
+            j = j+1
+        if self.slots[h] == None:
+            self.count += 1
+        self.slots[h] = item
+        self.check_growth()
+    
+    
 ht = HashTable() 
 ht.put("good", "eggs") 
 ht.put("better", "ham") 
@@ -79,3 +120,38 @@ ht.put("ga", "collide")
 ht.put("awd", "do not") 
 ht.put("add", "do not") 
 ht.check_growth()
+
+print("++++++++++++++++++")
+
+ht = HashTable()
+ht.put("good", "eggs")
+ht.put("better", "ham")
+ht.put("best", "spam")
+ht.put("ad", "do not")
+ht.put("ga", "collide")
+for key in ("good", "better", "best", "worst", "ad", "ga"):
+    v = ht.get(key)
+    print(v)
+
+#implement as dictionary
+print("///////////")
+ht = HashTable()
+ht["good"] = "eggs"
+ht["better"] = "ham"
+ht["best"] = "spam"
+ht["ad"] = "do not"
+ht["ga"] = "collide"
+for key in ("good", "better", "best", "worst", "ad", "ga"):
+    v = ht[key]
+    print(v)
+print("The number of elements is: {}".format(ht.count))
+
+#quadratic probing 
+print("--------------------------")
+ht = HashTable() 
+ht.put_quadratic("good", "eggs") 
+ht.put_quadratic("ad", "packt") 
+ht.put_quadratic("ga", "books") 
+ 
+v = ht.get_quadratic("ga") 
+print(v)
