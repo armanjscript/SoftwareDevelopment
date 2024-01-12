89a1dcbf36cf4b09b4ecace57b88a51b9a236e1f
armanjscript <armandaneshdoost@gmail.com>
2024-01-12 09:19:30 +0330

    Graphs.py changed

--- DataStructures/Section8/Graphs.py
+++ DataStructures/Section8/Graphs.py
@@ -30,3 +30,36 @@ for edge in edges_list:
 
 #print adjacency matrix
 print(adjacency_matrix)
+
+print("--------------->")
+
+#Breadth First Search
+graph = dict()
+graph['A'] = ['B', 'G', 'D']
+graph['B'] = ['A', 'F', 'E']
+graph['C'] = ['F', 'H']
+graph['D'] = ['F', 'A']
+graph['E'] = ['B', 'G']
+graph['F'] = ['B', 'D', 'C']
+graph['G'] = ['A', 'E']
+graph['H'] = ['C']
+
+from collections import deque
+
+def breadth_first_search(graph, root):
+    visited_vertices = list()
+    graph_deque = deque([root])
+    visited_vertices.append(root)
+    node = root
+    while len(graph_deque) > 0:
+        node = graph_deque.popleft()
+        adj_nodes = graph[node]
+        remaining_elements = set(adj_nodes).difference(set(visited_vertices))
+        if len(remaining_elements) > 0:
+            for elem in sorted(remaining_elements):
+                visited_vertices.append(elem)
+                graph_deque.append(elem)
+    return visited_vertices
+
+
+print(breadth_first_search(graph, 'A'))
